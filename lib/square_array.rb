adedcaedd951a63054804c4bdd6a82f7e6f4d853
megan <meggsnbacon777@gmail.com>
2020-07-04 00:52:05 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,13 @@
-def square_array(array)
- array=[1,2,3]
- new_array=[1,2,3]
-square_array(array)
-square_array(new_array)
+
 
   
 
 
+def square_array(array)
+  arr = []
+  array.each { |i| arr << i ** 2 }
+  arr
+end
+
+my_arr = [1, 2]
+p square_array(my_arr)
